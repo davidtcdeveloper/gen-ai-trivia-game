c6cae9c78d6913f3e5f447b1c60248c4b17044cb
David Tiago Conceição <davidtc.developer@gmail.com>
2025-12-04 09:37:12 -0300

    Firebase SDK setup and call on iOS

--- km-project/iosApp/iosApp/IosGeminiApi.swift
+++ km-project/iosApp/iosApp/IosGeminiApi.swift
@@ -1,14 +1,18 @@
+
 //
 // Created by David Tiago Conceição on 27/11/25.
 // Copyright (c) 2025 orgName. All rights reserved.
 //
 
-import Foundation
 import ComposeApp
+import FirebaseAILogic
+import Foundation
 
 class IosGeminiApi: GeminiApi {
     func getQuestions() async throws -> String {
-        //TODO invoke the actual API
-        "[]"
+        let ai = FirebaseAI.firebaseAI(backend: .googleAI())
+        let model = ai.generativeModel(modelName: "gemini-2.5-flash")
+        let response = try await model.generateContent(PromptKt.defaultQuestionsPrompt)
+        return response.text ?? ""
     }
 }
