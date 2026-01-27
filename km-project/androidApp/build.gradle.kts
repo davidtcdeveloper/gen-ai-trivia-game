f1612302266e8ed1f5ae8ed180619c0737f8a559
David Tiago Conceição <davidtc.developer@gmail.com>
2026-01-27 09:07:34 -0300

    Second step in AGP upgrade preparation

--- km-project/androidApp/build.gradle.kts
+++ km-project/androidApp/build.gradle.kts
@@ -1,7 +1,19 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    target {
+       compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
 }
 
 android {
@@ -32,13 +44,19 @@ android {
 }
 
 dependencies {
+    // Core components and lifecycle
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Jetpack compose
     implementation(platform(libs.androidx.compose.bom))
+    //TODO: review if these other compose dependencies are needed. Check if BOM is enough
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    // Firebase
+    implementation(project.dependencies.platform(libs.firebase.bom))
+    // Local project dependencies
     implementation(project(":composeApp"))
 }
