63c61304f6c66ded00db95e4b9fc7c7ae3f647e3
David Tiago Conceição <davidtc.developer@gmail.com>
2026-02-08 16:03:10 -0300

    Multiple updates

--- km-project/composeApp/build.gradle.kts
+++ km-project/composeApp/build.gradle.kts
@@ -36,18 +36,18 @@ kotlin {
     sourceSets {
 
         androidMain.dependencies {
-            implementation(compose.preview)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
             implementation(project.dependencies.platform(libs.firebase.bom))
         }
         commonMain.dependencies {
             // Jetpack Compose
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.runtime)
+            implementation(libs.foundation)
+            implementation(libs.material)
+            implementation(libs.ui)
+            implementation(libs.components.resources)
+            implementation(libs.ui.tooling.preview)
 
             // Lifecycle
             implementation(libs.androidx.lifecycle.viewmodel)
