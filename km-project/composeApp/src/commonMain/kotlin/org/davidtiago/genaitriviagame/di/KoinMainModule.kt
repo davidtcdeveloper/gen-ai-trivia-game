3ec7f230548d8696d2548e9f28bf23c9fcd1b1dd
David Tiago Conceição <davidtc.developer@gmail.com>
2026-02-11 09:35:45 -0300

    Adds TODO for specific module

--- km-project/composeApp/src/commonMain/kotlin/org/davidtiago/genaitriviagame/di/KoinMainModule.kt
+++ km-project/composeApp/src/commonMain/kotlin/org/davidtiago/genaitriviagame/di/KoinMainModule.kt
@@ -1,6 +1,7 @@
 package org.davidtiago.genaitriviagame.di
 
 import org.davidtiago.genaitriviagame.GameViewModel
+import org.davidtiago.genaitriviagame.repository.InMemoryMockQuestionRepository
 import org.davidtiago.genaitriviagame.repository.QuestionRepository
 import org.davidtiago.genaitriviagame.repository.gemini.GeminiQuestionRepository
 import org.koin.core.module.dsl.bind
@@ -9,7 +10,8 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val mainModule = module {
-    singleOf(::GeminiQuestionRepository){ bind<QuestionRepository>() }
-
+    //TODO: Separate this into a module for a different variant
+//    singleOf(::GeminiQuestionRepository){ bind<QuestionRepository>() }
+    singleOf(::InMemoryMockQuestionRepository){ bind<QuestionRepository>() }
     viewModelOf(::GameViewModel)
 }
