1fff4a45858958888e8e586d2fc63a1ebedad3d8
David Tiago Conceição <davidtc.developer@gmail.com>
2026-05-31 11:56:51 -0300

    Adjusting answer card layout

--- km-project/shared/src/commonMain/kotlin/org/davidtiago/genaitriviagame/ui/AnswerCardsUi.kt
+++ km-project/shared/src/commonMain/kotlin/org/davidtiago/genaitriviagame/ui/AnswerCardsUi.kt
@@ -1,6 +1,6 @@
 package org.davidtiago.genaitriviagame.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -8,36 +8,37 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun CorrectAnswerCard(selectedAnswer: String?) {
     Card(
+        modifier = Modifier.fillMaxWidth(),
         backgroundColor = Color.Green.copy(alpha = 0.1f),
-        modifier = Modifier.padding(top = 16.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                "Congratulations! '$selectedAnswer' is correct!",
-                color = Color.Green.copy(alpha = 0.8f)
-            )
-        }
+        Text(
+            "Congratulations! '$selectedAnswer' is correct!",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            color = Color.Green.copy(alpha = 0.8f)
+        )
     }
 }
 
 @Composable
 internal fun WrongAnswerCard(selectedAnswer: String?, correctAnswer: String) {
     Card(
+        modifier = Modifier.fillMaxWidth(),
         backgroundColor = Color.Red.copy(alpha = 0.1f),
-        modifier = Modifier.padding(top = 16.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                "Sorry, '$selectedAnswer' is incorrect. The correct answer is '$correctAnswer'.",
-                color = Color.Red.copy(alpha = 0.8f)
-            )
-        }
+        Text(
+            "Sorry, '$selectedAnswer' is incorrect. The correct answer is '$correctAnswer'.",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            color = Color.Red.copy(alpha = 0.8f)
+        )
     }
 }
 
--- km-project/shared/src/commonMain/kotlin/org/davidtiago/genaitriviagame/ui/ResultCardUi.kt
+++ km-project/shared/src/commonMain/kotlin/org/davidtiago/genaitriviagame/ui/ResultCardUi.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +27,10 @@ internal fun ResultCardUi(
         visible = true,
         enter = fadeIn() + expandVertically()
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
             if (isCorrect) {
                 CorrectAnswerCard(selectedAnswer)
             } else {
